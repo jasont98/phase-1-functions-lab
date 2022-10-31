098024083f533a67f7b641f3afea2c8199c9e35f
Hoang Tran <jashoang2583@gmail.com>
2022-10-31 14:26:22 -0700

    commit tree d4a88b92e9b078378c9e8a30258a1564df114366

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const distanceFromHqInBlocks = distance => {
+   return Math.abs(42 - distance);
+}
+const distanceFromHqInFeet = distance => {
+    return Math.abs(42 - distance) * 264;
+}
+function distanceTravelledInFeet(dist1, dist2) {
+    return Math.abs(dist1 - dist2) * 264;
+}
+
+function calculatesFarePrice(dist1, dist2) {
+    const distance = Math.abs(dist1 - dist2) * 264;
+    if (distance <= 400) {
+        return 0;
+    } else if (distance <= 2000) {
+        return (distance - 400) *.02;
+    } else if (distance <= 2500) {
+        return 25;
+    } else if (distance > 2501) {
+        return "cannot travel that far"
+    }
+}
